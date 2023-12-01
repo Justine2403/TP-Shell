5ac2a6971c061b671f0ca6bfb555abfff3c6b7f8
Justine2403 <justine@justine>
2023-12-01 10:56:58 +0100

    Question 2

---
+++ Q2/Q2.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <string.h>
+#include <time.h>
+
+#define BUFSIZE 128
+
+int main (int argc, char **argv[]){
+    char buffer[BUFSIZE];
+
+    // define fortune message
+    char *msg_fortune = "Today is what happened to yesterday.";
+    
+	// define the date
+	char *msg_date = "Sun Dec 13 13:19:40 CET 2020";
+
+	// define welcome message
+    char *msg_welcome = "Bienvenue dans le Shell ENSEA.\nPour quitter, tapez 'exit'.";
+    
+
+	// length of the string with strlen
+    write(1, msg_welcome, strlen(msg_welcome)); // fd = 1 is for Standard Output which is our terminal
+    
+	// show of simple prompt
+    char *msg_prompt = "\nenseash %";
+    write(1, msg_prompt, strlen(msg_prompt));
+
+    while(1){ //define an infinite loop
+        // waiting for an user to write a command 
+        read(0, buffer, BUFSIZE); //storing message input in buffer 
+
+        // Check if the input is fortune if yes write fortune message, 7 is the size of "fortune"
+        if (strncmp(buffer, "fortune", 7) == 0){
+            write(1, msg_fortune, strlen(msg_fortune));
+        }
+
+        // Else show the date 
+        else{
+			write(1, msg_date, strlen(msg_date));
+		}
+		// output of the prompt again 
+        write(1, msg_prompt, strlen(msg_prompt));
+		}		
+	}
+
